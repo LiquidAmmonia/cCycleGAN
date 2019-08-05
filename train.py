906312a51857230bd562949c8be69bac019da5e9
Kevin Chan <cya17@mails.tsinghua.edu.cn>
2019-08-05 22:27:40 +0800

    add train

--- train.py
+++ train.py
@@ -4,4 +4,7 @@ from data import create_dataset
 from models import create_model
 from util.visualizer import Visualizer
 
+if __name__ == '__main__':
+    opt = TrainOptions().parse()
+    dataset = create_dataset(opt)
 
